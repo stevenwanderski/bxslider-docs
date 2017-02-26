9a395cc931c33d0670b473f4844e0c467efca1e0
Steven Wanderski <steven.wanderski@gmail.com>
2017-02-26 16:07:17 -0600

    Changes option contol to a link

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,8 @@
 $(function(){
   $('.option__body').first().show();
 
-  $('.option__control').on('click', function(){
-    $(this).next('.option__body').toggle();
+  $('.option__control').on('click', function(e){
+    e.preventDefault();
+    $(this).parent().next('.option__body').toggle();
   });
 });
