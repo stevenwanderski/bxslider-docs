0d441f7a6ab1809f4409584c77676e57b8b96083
Steven Wanderski <steven.wanderski@gmail.com>
2017-10-11 09:07:50 -0500

    Adds better fonts

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -1,8 +1,22 @@
 $(function(){
+  // Options
   $('.option__body').first().show();
 
   $('.option__control').on('click', function(e){
     e.preventDefault();
     $(this).parent().next('.option__body').toggle();
+  });
+
+  // Tour
+  $('.tour__step-title').on('click', function(){
+    var $sibling = $(this).next('.tour__step-body');
+    $('.tour__step-body').not($sibling).hide();
+    $sibling.toggle();
+  });
+
+  $(document).on('click', function(e){
+    if (!$(e.target).is('.tour__step-title')) {
+      $('.tour__step-body').hide();
+    }
   });
 });
